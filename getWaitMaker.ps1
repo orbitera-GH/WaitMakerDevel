ee338206717905511dcc8c13a91089117f872d08
harn123 <harn@wp.pl>
2015-08-24 11:34:50 +0200

    poprawione ip devela

--- getWaitMaker.ps1
+++ getWaitMaker.ps1
@@ -3,10 +3,11 @@ $exit=0
 $log = "C:\Windows\Panther\getWaitMaker.log"
 $vmName=($env:computername).ToLower()
 $RG="orbiteratestdrive"+$vmName.Replace('gate','')
+$SupervisorIP = Get-Content -Path "c:\Windows\OEM\SuperVisorIP.txt"
 while ($ip.Length -lt 7) {
 	echo "Uploading resource group name...$RG " >> $log
 	   #(new-object net.webclient).DownloadString('http://168.62.183.34/azurecheck.php?group='+$RG)
-	$ip=(new-object net.webclient).DownloadString('http://168.62.183.34/azurecheck.php?group='+$RG)
+	$ip=(new-object net.webclient).DownloadString('http://'+$SupervisorIP+'/azurecheck.php?group='+$RG)
 	$z=$ip.Length
     date >> $log
 	echo "(WaitMaker) Length: $z ip: $ip" >> $log
@@ -18,7 +19,7 @@ echo "(WaitMaker) Script End" >> $log
 #$ip_port=$ip +":3395"
 $ip_port=$ip +":3389"
 echo "change NetAppStorage.RDP" >> C:\Windows\Panther\get.log
-$sqlName=(new-object net.webclient).DownloadString('http://168.62.183.34/azurecheck.php?group='+$RG+'&getsqlname=true')
+$sqlName=(new-object net.webclient).DownloadString('http://'+$SupervisorIP+'/azurecheck.php?group='+$RG+'&getsqlname=true')
 while ($exit -eq 0) {
     if (Test-Path -Path "C:\Users\Public\Desktop") {
 		if (!(Test-Path -Path "C:\Windows\OEM\NetAppStorage.RDP")) {
